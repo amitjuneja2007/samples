6efe6fc6ec5d3d0632637a6363224f9b996c54d9
amitjuneja2007 <amitjuneja2007@gmail.com>
2020-04-26 17:11:05 -0400

    Updated test cases with other options

--- src/test/java/com/company/test/jackson/JacksonTest.java
+++ src/test/java/com/company/test/jackson/JacksonTest.java
@@ -3,18 +3,32 @@ package com.company.test.jackson;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.company.test.java.model.SecurityData;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import lombok.Data;
+
 public class JacksonTest {
 
-	private ObjectMapper mapper = new ObjectMapper();
+	private static ObjectMapper mapper = new ObjectMapper();
 
 	private String FILE_NM = "test-data/data.json";
+
+	/**
+	 * Setup beans here
+	 */
+	@BeforeClass
+	public static void setup() {
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+	}
 
 	private String readJsonFromFile() throws IOException {
 		StringBuilder sb = new StringBuilder();
@@ -29,7 +43,7 @@ public class JacksonTest {
 	}
 
 	@Test
-	public void parseJsonString() throws IOException {
+	public void parseJsonToJavaObject() throws IOException {
 		/**
 		 * Enable pretty print
 		 */
@@ -46,5 +60,58 @@ public class JacksonTest {
 		 */
 		System.out.println(mapper.writeValueAsString(data));
 
+	}
+
+	@Test
+	public void parseJsonToJavaObject2() throws IOException {
+		/**
+		 * Enable pretty print
+		 */
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		String jsonString = readJsonFromFile();
+		SecurityData2 data = mapper.readValue(jsonString, SecurityData2.class);
+		/**
+		 * After parsing
+		 */
+		System.out.println(mapper.writeValueAsString(data));
+		data.getRequestHeader().setPropertyKey1("changedValue");
+		/**
+		 * Edit the Json
+		 */
+		System.out.println(mapper.writeValueAsString(data));
+
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	public void parseJsonToMap() throws IOException {
+
+		String jsonString = readJsonFromFile();
+
+		Map<String, Map<String, String>> jsonAsMap = (Map<String, Map<String, String>>) mapper.readValue(jsonString,
+				Map.class);
+		/**
+		 * After parsing
+		 */
+		System.out.println(mapper.writeValueAsString(jsonAsMap));
+		jsonAsMap.get("requestHeader").put("securityId", "abc");
+		/**
+		 * Edit the Json
+		 */
+		System.out.println(mapper.writeValueAsString(jsonAsMap));
+
+	}
+
+	@Data
+	private static class SecurityData2 {
+		private RequestHeader requestHeader;
+		private Map<String, String> securityInfo = new HashMap<String, String>();
+	}
+
+	@Data
+	private static class RequestHeader {
+		private String propertyKey1;
+		private String propertyKey2;
+		private String propertyKey3;
 	}
 }
